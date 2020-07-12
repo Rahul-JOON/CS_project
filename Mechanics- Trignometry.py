eb543b5ee066e533f2d63138cc3e182e4d0b2a1e
Rahul-JOON <61495262+Rahul-JOON@users.noreply.github.com>
2020-07-12 23:44:51 +0530

    Cleaned the code of physics model

--- Mechanics- Trignometry.py
+++ Mechanics- Trignometry.py
@@ -2,26 +2,32 @@ import pygame as p
 import math as m
 
 p.init()
+
 win = p.display.set_mode((500, 500))
 p.display.set_caption("Mechanics")
-x = 200
-y = 200
-radius = 40
-vel = 15
-deg = 0
-slantness = 0
 
 run = True
+
+class player():
+    def __init__(self, x , y, radius):
+        self.x = x 
+        self.y = y
+        self.radius = radius
+        self.vel = 15
+        self.deg = 0
+        self.slantness = 0
+
+tank = player(200, 200, 40)
 
 while run:
     p.time.delay(50)
 
-    print((x, y), round(m.sin(deg) * vel), m.cos(deg) * vel, deg)
+    print((tank.x, tank.y), round(m.sin(tank.deg) * tank.vel), m.cos(tank.deg) * tank.vel, tank.deg)
 
-    if deg > 360:
-        deg = 1
-    elif deg < 0:
-        deg = 359
+    if tank.deg > 360:
+        tank.deg = 1
+    elif tank.deg < 0:
+        tank.deg = 359
 
     for event in p.event.get():
         if event.type == p.QUIT:
@@ -29,19 +35,19 @@ while run:
     keys = p.key.get_pressed()
 
     if keys[p.K_UP]:
-        y -= round(m.cos(((m.pi)/180) * deg ) * vel)
-        x += round(m.sin(((m.pi)/180) * deg) * vel)
+        tank.y -= round(m.cos(((m.pi)/180) * tank.deg ) * tank.vel)
+        tank.x += round(m.sin(((m.pi)/180) * tank.deg) * tank.vel)
     if keys[p.K_DOWN]:
-        y += round(m.cos(((m.pi)/180) * deg) * vel)
-        x -= round(m.sin(((m.pi)/180) * deg) * vel)
+        tank.y += round(m.cos(((m.pi)/180) * tank.deg) * tank.vel)
+        tank.x -= round(m.sin(((m.pi)/180) * tank.deg) * tank.vel)
 
     if keys[p.K_RIGHT]:
-        deg += 15
+        tank.deg += 15
     if keys[p.K_LEFT]:
-        deg -= 15
+        tank.deg -= 15
 
     win.fill((0, 0, 0))
-    p.draw.circle(win, (155, 0, 0), (x,y), radius)
+    p.draw.circle(win, (155, 0, 0), (tank.x, tank.y), tank.radius)
     p.display.update()
 
 p.quit()
